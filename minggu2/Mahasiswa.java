127e02d0ef06b09c8629548892c8fcf6083475a8
Muhammad Thoriq Abrori <diagoreus17@gmail.com>
2017-02-28 11:34:15 +0100

    Add files via upload

---
+++ minggu2/Mahasiswa.java
@@ -0,0 +1,11 @@
+/*Menampilkan Data Mahasiswa*/
+
+public class Mahasiswa
+{
+  public static void main (String args [])
+ {
+   System.out.println("Nama Lengkap   : Muhammad Thoriq Abrori\n");
+   System.out.println("NIM            : 10115275\n"); 
+   System.out.println("Alamat         : Baleendah, Kabupaten Bandung"); 
+ }
+} 
